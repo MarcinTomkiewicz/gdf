e8a306648d29d70e98e0cb8c82835ebc2cc5bf9d
MarcinTomkiewicz <marc.tomk@gmail.com>
2022-07-03 09:39:22 +0200

    Deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Footer } from "./Footer/Footer";
+import { TopMenu } from "./TopMenu/TopMenu";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Program } from "./Program/Program";
+import { Home } from "./Home/Home";
+import { PartnerCarousel } from "./PartnerCarousel/PartnerCarousel";
 
 function App() {
+  const [content, setContent] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <TopMenu />
+      <main>
+        <Routes>
+          <Route path="/program" element={<Program />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <aside>
+          <PartnerCarousel type="patron" />
+        </aside>
+      </main>
+      <Footer />
     </div>
   );
 }
